e5b279977a8fba72c6212c9d33f2195a86090afa
PaolaRamos <r.o.l.pao@gmail.com>
2016-12-02 09:42:37 -0600

    nueva rama

---
+++ js/js.js
@@ -0,0 +1,28 @@
+ 
+$(document).ready(function(){
+        $('#button').click(function(){
+            $('.progressBar').each(function() {
+                var bar = $(this);
+                var maxvalue = $(this).attr('data');
+                maxvalue = 0;
+                var text = $(this).children('div').data('show');
+                progress1(maxvalue, bar, text);
+            });
+            $('.progressBar').each(function() {
+                var bar = $(this);
+                var maxvalue = $(this).attr('data');
+                maxvalue = maxvalue.substring(3);
+                var text = $(this).children('div').data('show');
+                progress(maxvalue, bar, text);
+            });
+        });                                    
+    });
+    
+    function progress1(percent, element, text) {
+        element.find('div').animate({ width: percent+'%' }, 1).html(text +"&nbsp;"+ percent + "%&nbsp;");
+    }
+    function progress(percent, element, text) {
+        element.find('div').animate({ width: percent+'%' }, 4000).html(text +"&nbsp;"+ percent + "%&nbsp;");
+    }
+
+
